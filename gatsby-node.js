cc4cd1508e2d3ccf3b52e828389ce3c2b3525188
thekabistro <kabir@kabirkuriyan.com>
2020-08-10 13:38:12 -0400

    Website Dev

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,54 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`) // highlight-line
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    const fileNode =
+      node.parent && node.parent !== 'undefined' ? getNode(node.parent) : node
+
+    createNodeField({
+      node,
+      name: `category`,
+      value: fileNode.relativeDirectory
+    })
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions // highlight-line
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  // highlight-start
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/md-holder.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug
+      }
+    })
+  })
+}
